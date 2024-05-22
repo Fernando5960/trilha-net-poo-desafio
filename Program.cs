cab672026306dfea9acfa170adb12f881969f995
Fernando <fernando.morais2005@outlook.com>
2024-05-22 11:20:16 -0300

    Finalização do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123", "Nokia 3310", "ABC1098", 2000);
+Iphone iphone = new Iphone("456", "IPhone 2", "DEF1234", 3000);
+
+nokia.InstalarAplicativo("Temple Run");
+iphone.InstalarAplicativo("Clash of Clans");
